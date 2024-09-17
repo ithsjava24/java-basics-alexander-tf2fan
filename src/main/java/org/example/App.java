47d4a54690dc35c6c60480ae47eb328e21ae7734
alexa <alexander.wettergren.stenders@iths.se>
2024-09-17 07:57:39 +0200

    Testerna för Main Menu gick igenom. Första steget mot ett "G" betyg färdigt.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.util.Locale;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        var locale = Locale.of("sv", "SE");
+        Locale.setDefault(locale);
+
+
+        String menu =  """
+                        Elpriser
+                        ========
+                        1. Inmatning
+                        2. Min, Max och Medel
+                        3. Sortera
+                        4. Bästa Laddningstid (4h)
+                        e. Avsluta
+                        """;
+
+        System.out.println(menu);
     }
 }
